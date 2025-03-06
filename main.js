9ba08fb991280bf5433c8b9cab4a52192abb1f69
Emílio Silva <emiliosarges@gmail.com>
2025-03-05 22:53:20 -0300

    retirada do código js inline

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play();
 }
 
+
 function tocaSomClap() {
     document.querySelector('#som_tecla_clap').play();
 }
@@ -34,6 +35,20 @@ function tocaSomTom() {
     document.querySelector('#som_tecla_tom').play();
 }
 
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
-document.querySelector('.#som_tecla_pom');
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
 
+document.querySelector('.tecla_tim').onclick = tocaSomTim;
+
+document.querySelector('.tecla_puff').onclick = tocaSomPuff;
+
+document.querySelector('.tecla_splash').onclick = tocaSomSplash;
+
+document.querySelector('.tecla_toim').onclick = tocaSomToim;
+
+document.querySelector('.tecla_psh').onclick = tocaSomPsh;
+
+document.querySelector('.tecla_tic').onclick = tocaSomTic;
+
+document.querySelector('.tecla_tom').onclick = tocaSomTom;
